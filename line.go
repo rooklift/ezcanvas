f63a8f69f4b73b7ac5021fb01ef4cfe1cd1d50e8
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2017-08-23 13:12:13 +0100

    use Bresenham line algo

--- line.go
+++ line.go
@@ -1,117 +1,162 @@
 package ezcanvas
 
+func abs(a int) int {
+	if a < 0 {
+		a *= -1
+	}
+	return a
+}
+
 func (c *Canvas) Line(r, g, b uint8, mode int, x1, y1, x2, y2 int) {
 
-    if x1 == x2 {
-        c.lineVertical(r, g, b, mode, x1, y1, y2)
-        return
-    }
-    if y1 == y2 {
-        c.lineHorizontal(r, g, b, mode, x1, y1, x2)
-        return
-    }
-
-    dx := x1 - x2
-    dy := y1 - y2
-    if dx < 0 { dx *= -1 }
-    if dy < 0 { dy *= -1 }
-
-    if dy < dx {
-        c.lineGentle(r, g, b, mode, x1, y1, x2, y2)
-    } else {
-        c.lineSteep(r, g, b, mode, x1, y1, x2, y2)
-    }
+	if x1 == x2 {
+		c.lineVertical(r, g, b, mode, x1, y1, y2)
+	} else if y1 == y2 {
+		c.lineHorizontal(r, g, b, mode, x1, y1, x2)
+	} else {
+		c.lineBresenham(r, g, b, mode, x1, y1, x2, y2)
+	}
 }
 
 func (c *Canvas) lineHorizontal(r, g, b uint8, mode int, x1, y, x2 int) {
 
-    if x1 > x2 {
-        x1, x2 = x2, x1
-    }
+	var sx int
 
-    for x := x1 ; x <= x2 ; x++ {
-        c.SetByMode(r, g, b, mode, x, y)
-    }
+	if x1 < x2 {
+		sx = 1
+	} else {
+		sx = -1
+	}
+
+	for {
+		c.SetByMode(r, g, b, mode, x1, y)
+		if x1 == x2 {
+			break
+		}
+		x1 += sx
+	}
 }
 
 func (c *Canvas) lineVertical(r, g, b uint8, mode int, x, y1, y2 int) {
 
-    if y1 > y2 {
-        y1, y2 = y2, y1
-    }
+	var sy int
 
-    for y := y1 ; y <= y2 ; y++ {
-        c.SetByMode(r, g, b, mode, x, y)
-    }
+	if y1 < y2 {
+		sy = 1
+	} else {
+		sy = -1
+	}
+
+	for {
+		c.SetByMode(r, g, b, mode, x, y1)
+		if y1 == y2 {
+			break
+		}
+		y1 += sy
+	}
 }
+
+func (c *Canvas) lineBresenham(r, g, b uint8, mode int, x1, y1, x2, y2 int) {
+
+	// http://members.chello.at/~easyfilter/bresenham.html
+
+	dx := abs(x2 - x1)
+	dy := abs(y2 - y1) * -1
+
+	var sx int
+	var sy int
+
+	if x1 < x2 { sx = 1 } else { sx = -1 }
+	if y1 < y2 { sy = 1 } else { sy = -1 }
+
+	err := dx + dy
+
+	for {
+		c.SetByMode(r, g, b, mode, x1, y1)
+		if (x1 == x2 && y1 == y2) { break }
+		e2 := 2 * err
+		if (e2 >= dy) {
+			err += dy
+			x1 += sx
+		}
+		if (e2 <= dx) {
+			err += dx
+			y1 += sy
+		}
+	}
+}
+
+/*
 
 func (c *Canvas) lineGentle(r, g, b uint8, mode int, x1, y1, x2, y2 int) {
 
-    // Based on an algorithm I read on the web 15 years ago;
-    // The webpage has long since vanished.
+	// Based on an algorithm I read on the web 15 years ago;
+	// The webpage has long since vanished.
 
-    var additive int
+	var additive int
 
-    if x1 > x2 {
-        x1, x2 = x2, x1
-        y1, y2 = y2, y1
-    }
+	if x1 > x2 {
+		x1, x2 = x2, x1
+		y1, y2 = y2, y1
+	}
 
-    if (y1 < y2) {
-        additive = 1;
-    } else {
-        additive = -1;
-    }
+	if (y1 < y2) {
+		additive = 1;
+	} else {
+		additive = -1;
+	}
 
-    dy_times_two := (y2 - y1) * 2
-    if dy_times_two < 0 { dy_times_two *= -1 }
+	dy_times_two := (y2 - y1) * 2
+	if dy_times_two < 0 { dy_times_two *= -1 }
 
-    dx_times_two := (x2 - x1) * 2       // We know we're going right, no need to check for < 0
+	dx_times_two := (x2 - x1) * 2       // We know we're going right, no need to check for < 0
 
-    the_error := x1 - x2
+	the_error := x1 - x2
 
-    for n := x1 ; n <= x2 ; n++ {
+	for n := x1 ; n <= x2 ; n++ {
 
-        c.SetByMode(r, g, b, mode, n, y1)
+		c.SetByMode(r, g, b, mode, n, y1)
 
-        the_error += dy_times_two;
-        if the_error > 0 {
-            y1 += additive
-            the_error -= dx_times_two
-        }
-    }
+		the_error += dy_times_two;
+		if the_error > 0 {
+			y1 += additive
+			the_error -= dx_times_two
+		}
+	}
 }
 
 func (c *Canvas) lineSteep(r, g, b uint8, mode int, x1, y1, x2, y2 int) {
 
-    var additive int
+	var additive int
 
-    if y1 > y2 {
-        x1, x2 = x2, x1
-        y1, y2 = y2, y1
-    }
+	if y1 > y2 {
+		x1, x2 = x2, x1
+		y1, y2 = y2, y1
+	}
 
-    if (x1 < x2) {
-        additive = 1;
-    } else {
-        additive = -1;
-    }
+	if (x1 < x2) {
+		additive = 1;
+	} else {
+		additive = -1;
+	}
 
-    dy_times_two := (y2 - y1) * 2       // We know we're going down, no need to check for < 0
+	dy_times_two := (y2 - y1) * 2       // We know we're going down, no need to check for < 0
 
-    dx_times_two := (x2 - x1) * 2
-    if dx_times_two < 0 { dx_times_two *= -1 }
+	dx_times_two := (x2 - x1) * 2
+	if dx_times_two < 0 { dx_times_two *= -1 }
 
-    the_error := y1 - y2;
+	the_error := y1 - y2;
 
-    for n := y1 ; n <= y2 ; n++ {
+	for n := y1 ; n <= y2 ; n++ {
 
-        c.SetByMode(r, g, b, mode, x1, n)
+		c.SetByMode(r, g, b, mode, x1, n)
 
-        the_error += dx_times_two
-        if the_error > 0 {
-            x1 += additive
-            the_error -= dy_times_two
-        }
-    }
+		the_error += dx_times_two
+		if the_error > 0 {
+			x1 += additive
+			the_error -= dy_times_two
+		}
+	}
 }
+
+*/
--- polygon_help.go
+++ polygon_help.go
@@ -4,175 +4,138 @@ package ezcanvas
 // Filled polygons work correctly for convex polygons only.
 
 type point struct {
-    x int
-    y int
+	x int
+	y int
 }
 
 type left_and_right struct {
-    left int
-    right int
+	left int
+	right int
 }
 
 type polygon struct {
-    edge_points map[point]bool          // All points that make up the edges of the polygon
-    extremes map[int]left_and_right     // Extreme left and right x values of the polygon for any given y value
+	edge_points map[point]bool          // All points that make up the edges of the polygon
+	extremes map[int]left_and_right     // Extreme left and right x values of the polygon for any given y value
 }
 
 func newPolygon() *polygon {
-    p := polygon{}
-    p.edge_points = make(map[point]bool)
-    p.extremes = make(map[int]left_and_right)
-    return &p
+	p := polygon{}
+	p.edge_points = make(map[point]bool)
+	p.extremes = make(map[int]left_and_right)
+	return &p
 }
 
 func (p *polygon) line(x1, y1, x2, y2 int) {
 
-    if x1 == x2 {
-        p.lineVertical(x1, y1, y2)
-        return
-    }
-    if y1 == y2 {
-        p.lineHorizontal(x1, y1, x2)
-        return
-    }
-
-    dx := x1 - x2
-    dy := y1 - y2
-    if dx < 0 { dx *= -1 }
-    if dy < 0 { dy *= -1 }
-
-    if dy < dx {
-        p.lineGentle(x1, y1, x2, y2)
-    } else {
-        p.lineSteep(x1, y1, x2, y2)
-    }
+	if x1 == x2 {
+		p.lineVertical(x1, y1, y2)
+	} else if y1 == y2 {
+		p.lineHorizontal(x1, y1, x2)
+	} else {
+		p.lineBresenham(x1, y1, x2, y2)
+	}
 }
 
 func (p *polygon) lineHorizontal(x1, y, x2 int) {
 
-    if x1 > x2 {
-        x1, x2 = x2, x1
-    }
+	var sx int
 
-    for x := x1 ; x <= x2 ; x++ {
-        p.set(x, y)
-    }
+	if x1 < x2 {
+		sx = 1
+	} else {
+		sx = -1
+	}
+
+	for {
+		p.set(x1, y)
+		if x1 == x2 {
+			break
+		}
+		x1 += sx
+	}
 }
 
 func (p *polygon) lineVertical(x, y1, y2 int) {
 
-    if y1 > y2 {
-        y1, y2 = y2, y1
-    }
+	var sy int
 
-    for y := y1 ; y <= y2 ; y++ {
-        p.set(x, y)
-    }
+	if y1 < y2 {
+		sy = 1
+	} else {
+		sy = -1
+	}
+
+	for {
+		p.set(x, y1)
+		if y1 == y2 {
+			break
+		}
+		y1 += sy
+	}
 }
 
-func (p *polygon) lineGentle(x1, y1, x2, y2 int) {
+func (p *polygon) lineBresenham(x1, y1, x2, y2 int) {
 
-    // Based on an algorithm I read on the web 15 years ago;
-    // The webpage has long since vanished.
+	// http://members.chello.at/~easyfilter/bresenham.html
 
-    var additive int
+	dx := abs(x2 - x1)
+	dy := abs(y2 - y1) * -1
 
-    if x1 > x2 {
-        x1, x2 = x2, x1
-        y1, y2 = y2, y1
-    }
+	var sx int
+	var sy int
 
-    if (y1 < y2) {
-        additive = 1;
-    } else {
-        additive = -1;
-    }
+	if x1 < x2 { sx = 1 } else { sx = -1 }
+	if y1 < y2 { sy = 1 } else { sy = -1 }
 
-    dy_times_two := (y2 - y1) * 2
-    if dy_times_two < 0 { dy_times_two *= -1 }
+	err := dx + dy
 
-    dx_times_two := (x2 - x1) * 2       // We know we're going right, no need to check for < 0
-
-    the_error := x1 - x2
-
-    for n := x1 ; n <= x2 ; n++ {
-
-        p.set(n, y1)
-
-        the_error += dy_times_two;
-        if the_error > 0 {
-            y1 += additive
-            the_error -= dx_times_two
-        }
-    }
-}
-
-func (p *polygon) lineSteep(x1, y1, x2, y2 int) {
-
-    var additive int
-
-    if y1 > y2 {
-        x1, x2 = x2, x1
-        y1, y2 = y2, y1
-    }
-
-    if (x1 < x2) {
-        additive = 1;
-    } else {
-        additive = -1;
-    }
-
-    dy_times_two := (y2 - y1) * 2       // We know we're going down, no need to check for < 0
-
-    dx_times_two := (x2 - x1) * 2
-    if dx_times_two < 0 { dx_times_two *= -1 }
-
-    the_error := y1 - y2;
-
-    for n := y1 ; n <= y2 ; n++ {
-
-        p.set(x1, n)
-
-        the_error += dx_times_two
-        if the_error > 0 {
-            x1 += additive
-            the_error -= dy_times_two
-        }
-    }
+	for {
+		p.set(x1, y1)
+		if (x1 == x2 && y1 == y2) { break }
+		e2 := 2 * err
+		if (e2 >= dy) {
+			err += dy
+			x1 += sx
+		}
+		if (e2 <= dx) {
+			err += dx
+			y1 += sy
+		}
+	}
 }
 
 func (p *polygon) set(x, y int) {
 
-    // Set a single point as being on the edge of the polygon,
-    // and update the data structure.
+	// Set a single point as being on the edge of the polygon,
+	// and update the data structure.
 
-    p.edge_points[point{x, y}] = true
+	p.edge_points[point{x, y}] = true
 
-    extremes, ok := p.extremes[y]
+	extremes, ok := p.extremes[y]
 
-    if ok == false {
+	if ok == false {
 
-        p.extremes[y] = left_and_right{x, x}
+		p.extremes[y] = left_and_right{x, x}
 
-    } else {
+	} else {
 
-        if extremes.left > x { extremes.left = x }
-        if extremes.right < x { extremes.right = x }
+		if extremes.left > x { extremes.left = x }
+		if extremes.right < x { extremes.right = x }
 
-        p.extremes[y] = extremes
-    }
+		p.extremes[y] = extremes
+	}
 }
 
 func (p *polygon) drawEdges(canvas *Canvas, r, g, b uint8, mode int) {
-    for point := range p.edge_points {
-        canvas.SetByMode(r, g, b, mode, point.x, point.y)
-    }
+	for point := range p.edge_points {
+		canvas.SetByMode(r, g, b, mode, point.x, point.y)
+	}
 }
 
 func (p *polygon) drawFilled(canvas *Canvas, r, g, b uint8, mode int) {
-    for y, extremes := range p.extremes {
-        for x := extremes.left ; x <= extremes.right ; x++ {
-            canvas.SetByMode(r, g, b, mode, x, y)
-        }
-    }
+	for y, extremes := range p.extremes {
+		for x := extremes.left ; x <= extremes.right ; x++ {
+			canvas.SetByMode(r, g, b, mode, x, y)
+		}
+	}
 }
